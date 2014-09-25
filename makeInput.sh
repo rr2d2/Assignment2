2b6de4e543bea779f3fed9fa235a1ad0052e7ef0
rr2d2 <vibam2010@yahoo.com>
2014-09-25 18:39:48 -0500

    sorts random input file
    sorts random input files and renames them so they can be used still
    have to go in and edit the top line of each file that is created and
    remove the “sorted sequence:”

---
+++ makeInput.sh
@@ -0,0 +1,24 @@
+echo “Script Started!”
+
+for a in S
+do
+  echo "Pass $a"
+  echo "---------------------"
+
+  # ===============================================
+
+  for b in Ran
+  do
+    echo "Pass $b"
+	for c in 1 2 3 4
+        do
+           ./sort -a $a -f input$b$c.txt -o InputInc$c.txt -p
+	done
+  done
+  # ===============================================
+  echo              # Space between output blocks in pass of outer loop.
+done
+
+exit 0
+echo “Script Ended!”
+
